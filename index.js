6cfdc64f7c47e12d0c91df729c5211f888cc5581
SedationH <sedationh@gmail.com>
2020-05-18 15:36:50 +0800

    🔥 then的核心,实现链式调用👀

--- index.js
+++ index.js
@@ -58,25 +58,59 @@ const
   }
 
   // then(等待并)处理状态改变后的Promise实例
-  // 还不能返回新的Promise
+  // 返回新的Promise，新状态由onResolved或者onRejected的返回值决定
   Promise.prototype.then = function (onResolved, onRejected) {
-    // 如果进入then的时候状态未发生改变，放入回调队列
-    if (this.state === PENDING) {
-      this.callbacks.push({
-        onResolved,
-        onRejected
-      })
-    } else if (this.state === FULFILLED) {
-      // 回调函数是异步执行
-      setTimeout(() => {
-        onResolved(this.data)
-      }, 0);
-    } else {
-      // 回调函数是异步执行
-      setTimeout(() => {
-        onRejected(this.data)
-      }, 0);
-    }
+
+    // 想要返回的新的Promise状态改变，要使用resove/reject,所以把整个代码放到里面
+    return new Promise((resovle, reject) => {
+      // 如果进入then的时候状态未发生改变，放入回调队列
+      if (this.state === PENDING) {
+        this.callbacks.push({
+          onResolved,
+          onRejected
+        })
+      } else if (this.state === FULFILLED) {
+        // 回调函数是异步执行
+        setTimeout(() => {
+          /**
+           * 对于结果的result的处理分三种情况
+           * 1. onResolved执行出错 -> 使用try-catch捕获 后 reject
+           * 2. result 为一个非Promise值 直接resolve(result)
+           * 3. result 为一个Promise值 那么将要返回的新Promise的值就是result执行的结果
+           */
+          try {
+            const result = onResolved(this.data)
+            if (result instanceof Promise) {
+              result.then(
+                value => resovle(value),
+                reason => reject(reason)
+              )
+            } else {
+              resovle(result)
+            }
+          } catch (reason) {
+            reject(reason)
+          }
+        }, 0);
+      } else {
+        // 回调函数是异步执行
+        setTimeout(() => {
+          try {
+            const result = onRejected(this.data)
+            if (result instanceof Promise) {
+              result.then(
+                value => resovle(value),
+                reason => reject(reason)
+              )
+            } else {
+              resovle(result)
+            }
+          } catch (reason) {
+            reject(reason)
+          }
+        }, 0);
+      }
+    })
   }
 
   window.Promise = Promise
